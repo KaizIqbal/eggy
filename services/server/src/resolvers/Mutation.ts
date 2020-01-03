c5c6cc6758ed3fdc05136a8f4b57746d4bce22de
Travis CI <travis@travis-ci.org>
2020-01-03 21:54:56 +0530

    ⬆️  Add Query and Mutation

--- services/server/src/resolvers/Mutation.ts
+++ services/server/src/resolvers/Mutation.ts
@@ -1,3 +1,19 @@
-const Mutation = {};
+const Mutation = {
+  async createEgg(parent, args, ctx, info) {
+    //TODO check it logged In
+    const egg = await ctx.db.mutation.createEgg(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    // console.log(egg);
+
+    return egg;
+  }
+};
 
 export default Mutation;
