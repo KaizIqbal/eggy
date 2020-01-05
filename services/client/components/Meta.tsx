bfc6d5291516d4697c6a86a6560106832dedc686
Travis CI <travis@travis-ci.org>
2020-01-05 10:30:34 +0530

    🔥 Setup Apollo Client

--- services/client/components/Meta.tsx
+++ services/client/components/Meta.tsx
@@ -5,7 +5,7 @@ const Meta = () => {
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta charSet="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       //TODO ADD favicon
       <title>Eggy</title>
     </Head>
