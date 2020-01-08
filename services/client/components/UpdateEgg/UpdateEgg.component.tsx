d84d0caa323b2f26cd5c4ece3c4293c28b92eef6
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-08 12:07:24 +0530

    🔥 Add action

--- services/client/components/UpdateEgg/UpdateEgg.component.tsx
+++ services/client/components/UpdateEgg/UpdateEgg.component.tsx
@@ -1,40 +1,65 @@
-import { useMutation } from "@apollo/react-hooks";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import * as React from "react";
 import { useForm } from "react-hook-form";
 import { Form } from "./UpdateEgg.styles";
 
-//Mutation for UpdateEgg
+// Query For fetch Egg by id
+const GET_EGG = gql`
+  query egg($id: ID!) {
+    egg(where: { id: $id }) {
+      id
+      title
+    }
+  }
+`;
+
+// Mutation for UpdateEgg
 const UPDATE_EGG_MUTATION = gql`
-  mutation UpdateEgg($title: String!) {
-    UpdateEgg(title: $title) {
+  mutation updateEgg($id: ID!, $title: String!) {
+    updateEgg(id: $id, title: $title) {
       id
     }
   }
 `;
 
-//UpdateEgg Component
+// UpdateEgg Component
 
 interface IUpdateEggProps {
   id: any;
 }
 
 const UpdateEgg: React.FunctionComponent<IUpdateEggProps> = props => {
-  //UpdateEgg Mutation hook
-  const [UpdateEgg, { loading, error }] = useMutation(UPDATE_EGG_MUTATION);
+  // Fetch data by id usinf Query Hook
+  const { loading: fetching, error: fetchingError, data: fetchData } = useQuery(
+    GET_EGG,
+    {
+      variables: { id: props.id }
+    }
+  );
+  // UpdateEgg Mutation hook
+  const [UpdateEgg, { loading, error }] = useMutation(UPDATE_EGG_MUTATION, {
+    onCompleted: data => {
+      console.log(data);
+    }
+  });
 
   // react form hook
   const { register, handleSubmit, errors } = useForm();
 
-  //Handle On Form Submit
+  // Handle On Form Submit
   const onSubmit = async (values, e) => {
     e.preventDefault();
     // console.log(values);
     // UpdateEgg Mutation call with data
-    await UpdateEgg({ variables: { title: values.title } });
+    await UpdateEgg({ variables: { id: props.id, title: values.title } });
   };
 
-  //rendering part
+  // rendering part
+  // Fetching Egg Details
+  if (fetching) return <p>Loading...</p>;
+  // if any error in fetching Data
+  if (fetchingError) return <p>Error: {fetchingError.message}</p>;
   // if any error in form submiting
   if (error) return <p>Error: {error.message}</p>;
 
@@ -45,6 +70,7 @@ const UpdateEgg: React.FunctionComponent<IUpdateEggProps> = props => {
         <label htmlFor="title">
           Title
           <input
+            defaultValue={fetchData.egg.title}
             type="text"
             id="title"
             name="title"
@@ -54,7 +80,7 @@ const UpdateEgg: React.FunctionComponent<IUpdateEggProps> = props => {
           {errors.title && "Your input is required"}
         </label>
         <br />
-        <button type="submit">Submit</button>
+        <button type="submit">Update</button>
       </fieldset>
     </Form>
   );
