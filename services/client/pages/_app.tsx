fbb81db75a8cf7434da2789037286e7fffc07af7
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-08 09:28:03 +0530

    🔣 Query type added

--- services/client/pages/_app.tsx
+++ services/client/pages/_app.tsx
@@ -35,7 +35,7 @@ interface IProps {
 class MyApp extends App<IProps> {
   // This is expose query to user
   static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+    let pageProps = { query: String };
 
     if (Component.getInitialProps) {
       pageProps = Component.getInitialProps(ctx);
