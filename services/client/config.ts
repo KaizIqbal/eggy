171c624a232e11bbf6c35c1b934a9497cba216c2
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-08 10:39:02 +0530

    ⚒️ Configure endpoint

--- services/client/config.ts
+++ services/client/config.ts
@@ -1,2 +1,2 @@
 export const endpoint = `http://localhost:4000`;
-export const codesandbox_server_endpoint = `https://8kcup.sse.codesandbox.io/`;
+export const codesandbox_server_endpoint = `https://nlcqm.sse.codesandbox.io/`;
