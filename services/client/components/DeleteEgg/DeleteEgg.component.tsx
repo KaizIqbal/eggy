9db19c64e92fcbdd8aa417044cf9d5f98e80e587
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-09 17:32:11 +0530

    🎊 Updated
    Updated CRUD Component
    Updated eggs page

--- services/client/components/DeleteEgg/DeleteEgg.component.tsx
+++ services/client/components/DeleteEgg/DeleteEgg.component.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import * as React from "react";
+import { EGGS_QUERY } from "../../pages/eggs";
 import { Button } from "./DeleteEgg.styles";
 
 // Mutation for DeleteEgg
@@ -13,17 +14,29 @@ const DELETE_EGG_MUTATION = gql`
 `;
 
 // DeleteEgg Component
-
 interface IDeleteEggProps {
   children: any;
   id: any;
 }
 
 const DeleteEgg: React.FunctionComponent<IDeleteEggProps> = props => {
+  //Manually Update  the cache on the client,so ot matches the server
+  const update = (Cache, payload) => {
+    // 1. Read the cache for the eggs we want
+    const data = Cache.readQuery({ query: EGGS_QUERY });
+    // console.log(data, payload);
+    // 2. filter the deleted egg to the list
+    data.eggs = data.eggs.filter(egg => egg.id !== payload.data.deleteEgg.id);
+    // 3.put the eggs back
+    Cache.writeQuery({ query: EGGS_QUERY, data });
+  };
+
   // DeleteEgg Mutation hook
   const [deleteEgg, { error }] = useMutation(DELETE_EGG_MUTATION, {
-    variables: { id: props.id }
+    variables: { id: props.id },
+    update: update
   });
+
   //Handle onClick
   const onClick = () => {
     if (window.confirm("Are you sure you want to delete this egg?")) {
