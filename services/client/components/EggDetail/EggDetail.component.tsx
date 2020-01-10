4ec4b7dd84fbc29634846d2e3b19c8d5e8b7e899
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-10 22:04:25 +0530

    🥚 Display Egg detail

--- services/client/components/EggDetail/EggDetail.component.tsx
+++ services/client/components/EggDetail/EggDetail.component.tsx
@@ -1,4 +1,6 @@
+import { useQuery } from "@apollo/react-hooks";
 import * as React from "react";
+import { EGG_QUERY } from "../../graphql/Query";
 
 // DeleteEgg Component
 interface IDeleteEggProps {
@@ -6,7 +8,25 @@ interface IDeleteEggProps {
 }
 
 const DeleteEgg: React.FunctionComponent<IDeleteEggProps> = props => {
-  return <p>This is Egg Detail Page : {props.id}</p>;
+  // Fetch data by id using Query Hook
+  const { loading, error, data } = useQuery(EGG_QUERY, {
+    variables: { id: props.id }
+  });
+
+  // rendering part
+  // Fetching Egg Details
+  if (loading) return <p>Loading...</p>;
+  // if any error in fetching Data
+  if (error) return <p>Error: {error.message}</p>;
+  // if Data is not existed
+  if (!data.egg) return <p>No Egg Found</p>;
+
+  return (
+    <div>
+      <h1>Egg Detail</h1>
+      <h3>{data.egg.title}</h3>
+    </div>
+  );
 };
 
 export default DeleteEgg;
