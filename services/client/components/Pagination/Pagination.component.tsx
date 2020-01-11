666e5d502be58ff2cf2d49b95da2721ae2cfef1c
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-11 09:58:29 +0530

    ➕ Render Page

--- services/client/components/Pagination/Pagination.component.tsx
+++ services/client/components/Pagination/Pagination.component.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@apollo/react-hooks";
 import * as React from "react";
 import { PAGINATION_QUERY } from "../../graphql/Query";
 import PaginationStyle from "./Pagination.styles";
+import { perPage } from "../../config";
 
 // Pagination Component
 interface IPaginationProps {}
@@ -12,16 +13,16 @@ const Pagination: React.FunctionComponent<IPaginationProps> = props => {
   // console.log(data);
 
   // rendering part
+
   // Fetching Egg Details
   if (loading) return <p>Loading...</p>;
   // if any error in fetching Data
   if (error) return <p>Error: {error.message}</p>;
 
-  return (
-    <PaginationStyle>
-      Pagination {data.eggsConnection.aggregate.count}
-    </PaginationStyle>
-  );
+  ///Total count return by query
+  const count = data.eggsConnection.aggregate.count;
+  const page = Math.ceil(count / perPage);
+  return <PaginationStyle>Page 1 Of {page}</PaginationStyle>;
 };
 
 export default Pagination;
