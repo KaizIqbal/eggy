21efdf3c2f1c64ebeb90f6c045283b24eeee2e66
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-11 10:10:25 +0530

    🔄 Put query in Component

--- services/client/pages/basket.tsx
+++ services/client/pages/basket.tsx
@@ -1,24 +1,19 @@
-import { useQuery } from "@apollo/react-hooks";
-import { CreateEgg, EggList ,Pagination} from "../components";
-import {EGGS_QUERY} from "../graphql/Query"
+import { CreateEgg, EggList, Pagination } from "../components";
+import { EGGS_QUERY } from "../graphql/Query";
 
 //Eggs page
 const Basket = props => {
-  // the hook that calls the query.
-  const eggs = useQuery(EGGS_QUERY);
-  // console.log(eggs);
   return (
     <div>
       <h1>Create Egg Form</h1>
       <CreateEgg />
       <h1>Eggs</h1>
       <Pagination />
-      <EggList eggs={eggs?.data?.eggs||[]} />
-      <Pagination />  
+      <EggList />
+      <Pagination />
     </div>
   );
 };
 
 export default Basket;
 export { EGGS_QUERY };
-
