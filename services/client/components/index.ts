0abd4f38ad004f1f9a657fa27486c79bbb349a0a
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-11 08:39:33 +0530

    ➕ Add Pagination Component

--- services/client/components/index.ts
+++ services/client/components/index.ts
@@ -1,7 +1,8 @@
 import CreateEgg from "./CreateEgg/CreateEgg.component";
+import EggDetail from "./EggDetail/EggDetail.component";
 import EggList from "./EggList/EggList.component";
 import Page from "./Page/Page.component";
+import Pagination from "./Pagination/Pagination.component";
 import UpdateEgg from "./UpdateEgg/UpdateEgg.component";
-import EggDetail from "./EggDetail/EggDetail.component";
 
-export { CreateEgg, Page, UpdateEgg, EggList, EggDetail };
+export { CreateEgg, EggDetail, EggList, Page, Pagination, UpdateEgg };
