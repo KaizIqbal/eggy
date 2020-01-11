ee10f17b5002564afbe17c0a83fae272a525c766
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-11 10:10:25 +0530

    🔄 Put query in Component

--- services/client/components/EggList/EggList.component.tsx
+++ services/client/components/EggList/EggList.component.tsx
@@ -1,8 +1,10 @@
+import { useQuery } from "@apollo/react-hooks";
+import Link from "next/link";
 import Router from "next/router";
 import React from "react";
+import { EGGS_QUERY } from "../../graphql/Query";
 import DeleteEgg from "../DeleteEgg/DeleteEgg.component";
 import { Button, List, ListEgg } from "./EggList.styles";
-import Link from "next/link";
 
 // this interface defines the shape of the data returned by the eggs query.
 export interface Iegg {
@@ -10,12 +12,22 @@ export interface Iegg {
   title: string;
 }
 
-interface IProps {
-  eggs: Iegg[];
-}
+// DeleteEgg Component
+interface IEggListProps {}
 
-const EggList = ({ eggs }: IProps) => {
-  const listEgg = eggs.map(egg => {
+const EggList: React.FunctionComponent<IEggListProps> = props => {
+  // the hook that calls the query.
+  const { loading, error, data } = useQuery(EGGS_QUERY);
+
+  // rendering part
+  // Fetching Egg Details
+  if (loading) return <p>Loading...</p>;
+  // if any error in fetching Data
+  if (error) return <p>Error: {error.message}</p>;
+  // if Data is not existed
+  if (!data.eggs) return <p>No Egg Found</p>;
+
+  const listEgg = data.eggs.map(egg => {
     return (
       <ListEgg key={egg.id}>
         <Link href={{ pathname: "/egg", query: { id: egg.id } }}>
