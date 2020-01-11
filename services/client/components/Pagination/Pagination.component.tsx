59a892a36bb7c5effbad8426418b635be77d8d67
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-11 09:50:31 +0530

    🔼 fetch data in Pagination
    - and update all packages

--- services/client/components/Pagination/Pagination.component.tsx
+++ services/client/components/Pagination/Pagination.component.tsx
@@ -1,11 +1,27 @@
+import { useQuery } from "@apollo/react-hooks";
 import * as React from "react";
+import { PAGINATION_QUERY } from "../../graphql/Query";
 import PaginationStyle from "./Pagination.styles";
 
 // Pagination Component
 interface IPaginationProps {}
 
 const Pagination: React.FunctionComponent<IPaginationProps> = props => {
-  return <PaginationStyle>Pagination</PaginationStyle>;
+  // Fetch data by id using Query Hook
+  const { loading, error, data } = useQuery(PAGINATION_QUERY);
+  // console.log(data);
+
+  // rendering part
+  // Fetching Egg Details
+  if (loading) return <p>Loading...</p>;
+  // if any error in fetching Data
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <PaginationStyle>
+      Pagination {data.eggsConnection.aggregate.count}
+    </PaginationStyle>
+  );
 };
 
 export default Pagination;
