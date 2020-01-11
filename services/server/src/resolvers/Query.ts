37933963d700b7693b54bf893dec52ddafc00bd0
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-11 08:47:04 +0530

    ➕ Add EggConnection
    For Pagination

--- services/server/src/resolvers/Query.ts
+++ services/server/src/resolvers/Query.ts
@@ -2,7 +2,8 @@ import { forwardTo } from "prisma-binding";
 
 const Query = {
   eggs: forwardTo("db"),
-  egg: forwardTo("db")
+  egg: forwardTo("db"),
+  eggsConnection: forwardTo("db")
 };
 
 export default Query;
