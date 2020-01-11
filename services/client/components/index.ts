89dd51473b79da53420482e9983cabc1a1e79803
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-11 10:22:20 +0530

    📑 Manual Pagination

--- services/client/components/index.ts
+++ services/client/components/index.ts
@@ -2,7 +2,6 @@ import CreateEgg from "./CreateEgg/CreateEgg.component";
 import EggDetail from "./EggDetail/EggDetail.component";
 import EggList from "./EggList/EggList.component";
 import Page from "./Page/Page.component";
-import Pagination from "./Pagination/Pagination.component";
 import UpdateEgg from "./UpdateEgg/UpdateEgg.component";
 
-export { CreateEgg, EggDetail, EggList, Page, Pagination, UpdateEgg };
+export { CreateEgg, EggDetail, EggList, Page, UpdateEgg };
