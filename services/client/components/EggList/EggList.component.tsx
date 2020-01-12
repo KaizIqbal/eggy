7dd3e5373a284b077c6cf6d8b291cd31de439f74
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-12 10:38:49 +0530

    🧹 cleaning up

--- services/client/components/EggList/EggList.component.tsx
+++ services/client/components/EggList/EggList.component.tsx
@@ -1,72 +1,25 @@
-import { useQuery } from "@apollo/react-hooks";
-import Link from "next/link";
-import Router from "next/router";
 import React from "react";
-import { EGGS_QUERY } from "../../graphql/Query";
-import DeleteEgg from "../DeleteEgg/DeleteEgg.component";
-import { Button, List, ListEgg } from "./EggList.styles";
-import { perPage } from "../../config";
+import useEggs from "./EggList.hooks";
 
 // ##### COMPONENT PROPS TYPE #####
-interface IEggListProps {
-  page: number;
-}
+
+interface IEggListProps {}
 
 // ##### COMPONENT #####
-const EggList: React.FunctionComponent<IEggListProps> = props => {
-  // ##### HOOKS #####
 
-  // the hook that calls the query.
-  const { loading, error, data } = useQuery(EGGS_QUERY, {
-    variables: { skip: props.page * perPage - perPage }
-  });
+const EggList: React.FunctionComponent<IEggListProps> = props => {
+  // ##### CUSTOM HOOKS #####
+
+  const { eggs, loading, loadMore, hasNextPage } = useEggs();
+
+  // ##### VARIABLES #####
+
+  const eggsCount = hasNextPage ? eggs.length + 1 : eggs.length;
+  const loadMoreEggs = loading ? () => {} : loadMore;
+  const isEggLoaded = index => !hasNextPage || index < eggs.length;
 
   // ##### RENDER #####
-
-  // Fetching Eggs Details
-  if (loading) return <p>Loading...</p>;
-
-  // if any error in fetching Data
-  if (error) return <p>Error: {error.message}</p>;
-
-  // if Data is not existed
-  if (!data.eggs) return <p>No Egg Found</p>;
-
-  // create List of each egg
-  const listEgg = data.eggs.map(egg => {
-    return (
-      <ListEgg key={egg.id}>
-        {/* Name of Egg with Link of Detail Page*/}
-        <Link prefetch href={{ pathname: "/egg", query: { id: egg.id } }}>
-          <a>{egg.title}</a>
-        </Link>
-
-        {/*  Edit Button*/}
-        <Button
-          type="button"
-          name="Edit"
-          value="Edit"
-          onClick={() => {
-            Router.push({
-              pathname: "/update",
-              query: { id: egg.id }
-            });
-          }}>
-          Edit
-        </Button>
-
-        {/* Delete Button */}
-        <DeleteEgg id={egg.id}>Delete</DeleteEgg>
-      </ListEgg>
-    );
-  });
-
-  return (
-    <>
-      {/* Actual Egg List */}
-      <List>{listEgg}</List>
-    </>
-  );
+  return <></>;
 };
 
 export default EggList;
--- services/client/components/EggList/EggList.hooks.ts
+++ services/client/components/EggList/EggList.hooks.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/react-hooks";
-import { GET_EGGS_CURSOR } from "../../graphql/Query";
+import { EGGS_QUERY, GET_EGGS_CURSOR } from "../../graphql/Query";
 
 function useEggs() {
   const { data, loading, fetchMore } = useQuery(GET_EGGS_CURSOR, {
@@ -7,4 +7,33 @@ function useEggs() {
   });
 
   if (loading && !data.eggs) return { loading, eggs: [] };
+
+  const loadMore = () => {
+    return fetchMore({
+      query: EGGS_QUERY,
+      variables: {
+        cursor: data.eggs.pageInfo.endCursor
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        const newEdges = fetchMoreResult.eggs.edges;
+        const pageInfo = fetchMoreResult.eggs.pageInfo;
+        return newEdges.length
+          ? {
+              eggs: {
+                __typename: previousResult.eggs.__typename,
+                edges: [...previousResult.eggs.edges, ...newEdges],
+                pageInfo
+              }
+            }
+          : previousResult;
+      }
+    });
+  };
+  return {
+    eggs: data.eggs.edges.map(({ node }) => node),
+    hasNextPage: data.eggs.pageInfo.hasNextPage,
+    loading,
+    loadMore
+  };
 }
+export default useEggs;
