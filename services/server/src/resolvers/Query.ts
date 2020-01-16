7b1f85cab97e102defe2201048fc3eae2fa552fa
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-16 12:49:29 +0530

    🛠️ Modify request

--- services/server/src/resolvers/Query.ts
+++ services/server/src/resolvers/Query.ts
@@ -3,7 +3,19 @@ import { forwardTo } from "prisma-binding";
 const Query = {
   eggs: forwardTo("db"),
   egg: forwardTo("db"),
-  eggsConnection: forwardTo("db")
+  eggsConnection: forwardTo("db"),
+  me(parent, args, ctx, info) {
+    // check if there is current user ID
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 export default Query;
