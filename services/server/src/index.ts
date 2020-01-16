3a91e18fe97b729c7fcb23d4f58fdd5d0aaebec8
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-16 12:49:29 +0530

    🛠️ Modify request

--- services/server/src/index.ts
+++ services/server/src/index.ts
@@ -2,13 +2,24 @@
 // require("dotenv").config({ path: "secret.env" });
 import * as coockieParser from "cookie-parser";
 import createServer from "./createServer";
+import * as jwt from "jsonwebtoken";
 import db from "./db";
+import { Interface } from "readline";
 
 const server = createServer();
 
 server.express.use(coockieParser());
-// TODO use express middleware to produce current user
+// Decode the token to get userId from each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
 
+    // put the userId onto the req for the further requests to access
+    req.userId = userId;
+  }
+  next();
+});
 server.start(
   {
     cors: {
