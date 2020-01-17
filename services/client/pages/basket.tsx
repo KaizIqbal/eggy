be0f889d0942577276ccfed12e7bfccd10f57ae1
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-17 15:56:44 +0530

    🧹 Code cleanup

--- services/client/pages/basket.tsx
+++ services/client/pages/basket.tsx
@@ -1,6 +1,4 @@
 import { CreateEgg, EggList } from "../components";
-import { EGGS_QUERY } from "../graphql/Query";
-
 //##### PAGE #####
 const Basket = props => {
   return (
@@ -14,4 +12,3 @@ const Basket = props => {
 };
 
 export default Basket;
-export { EGGS_QUERY };
