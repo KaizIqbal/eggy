7d84c4cbe39c7f5e239ed42dc53e65592feabd60
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-22 21:10:22 +0530

    ➕ add more pages to the basket and login

--- services/client/hooks/user.ts
+++ services/client/hooks/user.ts
@@ -1,8 +1,10 @@
 import { useQuery } from "@apollo/react-hooks";
-import { ME_QUERY } from "../../../graphql/Query";
+import { ME_QUERY } from "../graphql/Query";
 
 function useUser() {
-  const { data, loading, error } = useQuery(ME_QUERY);
+  const { data, loading, error } = useQuery(ME_QUERY, {
+    fetchPolicy: "cache-first"
+  });
 
   if (loading) return { loading };
   if (error) return { error };
--- services/client/pages/basket/index.tsx
+++ services/client/pages/basket/index.tsx
@@ -1,10 +1,8 @@
-import { CreateEgg, EggList } from "../components";
+import { EggList } from "../../components";
 //##### PAGE #####
 const Basket = props => {
   return (
     <div>
-      <h1>Create Egg Form</h1>
-      <CreateEgg />
       <h1>Eggs</h1>
       <EggList />
     </div>
