1dedcc4bfebd554639c21bb19aa9ff05c586ad1e
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-25 08:50:42 +0530

    🔒 LogIn require to add Egg

--- services/client/components/layout/PleaseLogIn/PleaseLogIn.component.tsx
+++ services/client/components/layout/PleaseLogIn/PleaseLogIn.component.tsx
@@ -1,2 +1,26 @@
-const PleaseLogIn = props => props.children;
+import useUser from "../../../hooks/user";
+import React from "react";
+
+// ##### COMPONENT PROPS TYPE #####
+interface IPleaseLogInProps {
+  children: any;
+}
+
+// ##### COMPONENT #####
+const PleaseLogIn: React.FunctionComponent<IPleaseLogInProps> = props => {
+  // ##### HOOKS #####
+
+  const { me, loading, error } = useUser();
+
+  // ##### RENDER #####
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error! ${error.message}</p>;
+
+  if (!me) {
+    return <p>Please Login to Continue</p>;
+  }
+
+  return props.children;
+};
 export default PleaseLogIn;
