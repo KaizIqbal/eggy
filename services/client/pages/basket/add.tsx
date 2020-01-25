d631ec818bb300e5b1c946b1e3cbf85ff3269f48
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-25 08:50:42 +0530

    🔒 LogIn require to add Egg

--- services/client/pages/basket/add.tsx
+++ services/client/pages/basket/add.tsx
@@ -1,31 +1,13 @@
 import { CreateEgg, PleaseLogIn } from "../../components";
-import useUser from "../../hooks/user";
 
 // ##### PAGE #####
-const AddEggPage = ({ query }) => {
-  // ##### HOOKS #####
-
-  const { me, loading, error } = useUser();
-
-  // ##### RENDER #####
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error! ${error.message}</p>;
-  console.log(me);
-  if (!me) {
-    return <p>Please Login</p>;
-  }
-
-  if (me) {
-    return (
-      <div>
-        <PleaseLogIn>
-          <h1>Create Egg Form</h1>
-          <CreateEgg />
-        </PleaseLogIn>
-      </div>
-    );
-  }
-};
+const AddEggPage = () => (
+  <div>
+    <PleaseLogIn>
+      <h1>Create Egg Form</h1>
+      <CreateEgg />
+    </PleaseLogIn>
+  </div>
+);
 
 export default AddEggPage;
