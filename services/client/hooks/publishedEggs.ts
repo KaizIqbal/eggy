e03fdbe3d26611b6f2a3c602de045bd3e5b8a3ed
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-01-30 09:32:32 +0530

    💡 Separate eggs query by published and user

--- services/client/hooks/publishedEggs.ts
+++ services/client/hooks/publishedEggs.ts
@@ -2,18 +2,18 @@ import { useQuery } from "@apollo/react-hooks";
 import { GET_EGGS_CURSOR } from "../graphql/Query";
 import updateEggCache from "../utils/updateEggCache";
 
-function useEggs() {
+function usePublishedEggs() {
   const { data, loading, fetchMore, error } = useQuery(GET_EGGS_CURSOR);
 
   if (loading) return { loading };
   if (error) return { error };
-  if (loading && !data.eggsConnection) return { loading, eggs: [] };
+  if (loading && !data.publishedEggsConnection) return { loading, eggs: [] };
 
   const loadMore = () => {
     return fetchMore({
       query: GET_EGGS_CURSOR,
       variables: {
-        cursor: data.eggsConnection.pageInfo.endCursor
+        cursor: data.publishedEggsConnection.pageInfo.endCursor
       },
       updateQuery: (previousResult, { fetchMoreResult }) => {
         return updateEggCache(fetchMoreResult, previousResult);
@@ -21,11 +21,11 @@ function useEggs() {
     });
   };
   return {
-    eggs: data.eggsConnection.edges.map(({ node }) => node),
-    hasNextPage: data.eggsConnection.pageInfo.hasNextPage,
+    eggs: data.publishedEggsConnection.edges.map(({ node }) => node),
+    hasNextPage: data.publishedEggsConnection.pageInfo.hasNextPage,
     loading,
     loadMore,
     error
   };
 }
-export default useEggs;
+export default usePublishedEggs;
--- services/client/hooks/userEggs.ts
+++ services/client/hooks/userEggs.ts
@@ -2,19 +2,18 @@ import { useQuery } from "@apollo/react-hooks";
 import { GET_USER_EGGS_CURSOR } from "../graphql/Query";
 import updateEggCache from "../utils/updateEggCache";
 
-function useUserEggs({ userId }) {
+function useUserEggs() {
   const { data, loading, fetchMore, error } = useQuery(GET_USER_EGGS_CURSOR);
 
   if (loading) return { loading };
   if (error) return { error };
-  if (loading && !data.eggsConnection) return { loading, eggs: [] };
+  if (loading && !data.userEggsConnection) return { loading, eggs: [] };
 
   const loadMore = () => {
     return fetchMore({
       query: GET_USER_EGGS_CURSOR,
       variables: {
-        cursor: data.eggsConnection.pageInfo.endCursor,
-        userId: userId
+        cursor: data.userEggsConnection.pageInfo.endCursor
       },
       updateQuery: (previousResult, { fetchMoreResult }) => {
         return updateEggCache(fetchMoreResult, previousResult);
@@ -22,8 +21,8 @@ function useUserEggs({ userId }) {
     });
   };
   return {
-    eggs: data.eggsConnection.edges.map(({ node }) => node),
-    hasNextPage: data.eggsConnection.pageInfo.hasNextPage,
+    eggs: data.userEggsConnection.edges.map(({ node }) => node),
+    hasNextPage: data.userEggsConnection.pageInfo.hasNextPage,
     loading,
     loadMore,
     error
