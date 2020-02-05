3bceb341f528ba4a9dcd4a0f51e42fe5a34d5e8e
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-02-05 09:29:48 +0530

    ➕ Cursors query

--- services/server/src/resolvers/Query.ts
+++ services/server/src/resolvers/Query.ts
@@ -4,6 +4,7 @@ const { loggedIn } = require("../utils/loggedIn");
 
 const Query = {
   egg: forwardTo("db"),
+  cursors: forwardTo("db"),
   userEggsConnection(parent, args, ctx, info) {
     // Checking user logged in or not if not then throw Error
     loggedIn(ctx);
