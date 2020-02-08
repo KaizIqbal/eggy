96a11bcda7861970ccdb5b9db5a7dacb946cb840
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-02-08 17:39:55 +0530

    ➕ CursorWorkshop added

--- services/client/components/workshops/Cursor.tsx
+++ services/client/components/workshops/Cursor.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { FLAVOR_QUERY } from "../../graphql/Query";
 
 // ##### COMPONENT PROPS TYPE #####
 
@@ -13,12 +15,22 @@ interface ICursorWorkshopProps {
 const CursorWorkshop: React.FunctionComponent<ICursorWorkshopProps> = props => {
   // ##### HOOKS #####
 
+  const { data, loading, error } = useQuery(FLAVOR_QUERY, {
+    variables: {
+      name: props.flavorname
+    }
+  });
+
   // ##### RENDER #####
+  if (loading) return <p>Fetching Falvor......</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  if (!data) return <p>Flavor not found</p>;
 
   return (
     <>
       <h1>
-        {props.username}'s Workshop for {props.eggname} {props.flavorname}
+        {props.username}'s Workshop for {props.eggname} {data.flavor.name}
       </h1>
     </>
   );
