c0193865a57064c14cd3accf0649738297506a9b
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-02-14 16:57:18 +0530

    ☁️ Add Storage API with S3

--- services/server/src/modules/fileApi.ts
+++ services/server/src/modules/fileApi.ts
@@ -1,54 +1,73 @@
 import * as uuid from "uuid";
+const AWS = require("aws-sdk");
 
-import AWS from "aws-sdk";
+function uploadToS3(file: { filename: any; stream: any }) {
+  const s3bucket = new AWS.S3({
+    accessKeyId: process.env.IAM_USER_KEY,
+    secretAccessKey: process.env.IAM_USER_SECRET,
+    Bucket: process.env.BUCKET_NAME
+  });
+  s3bucket.createBucket(function() {
+    const key = uuid() + "-" + file.filename;
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.S3_KEY,
-  secretAccessKey: process.env.S3_SECRET,
-  params: {
-    Bucket: process.env.S3_BUCKET
-  }
-});
-
-exports.processUpload = async (upload, ctx) => {
-  if (!upload) {
-    return console.log("ERROR: No file received.");
-  }
-
-  const { stream, filename, mimetype, encoding } = await upload;
-  const key = uuid() + "-" + filename;
-
-  // Upload to S3
-  const response = await s3
-    .upload({
+    const params = {
+      Bucket: process.env.BUCKET_NAME,
       Key: key,
-      ACL: "public-read",
-      Body: stream
-    })
-    .promise();
+      Body: file.stream
+    };
 
-  const url = response.Location;
+    s3bucket.upload(params, function(err: any, data: any) {
+      if (err) {
+        console.log("error in callback");
+        console.log(err);
+      }
+      console.log("success");
+      console.log(data);
+    });
+  });
+}
 
-  // Sync with Prisma
-  const data = {
-    filename,
-    mimetype,
-    encoding,
-    url
-  };
+export default uploadToS3;
 
-  const { id } = await ctx.db.mutation.createFile({ data }, ` { id } `);
+// const s3 = new AWS.S3({
+//   accessKeyId: process.env.IAM_USER_KEY,
+//   secretAccessKey: process.env.IAM_USER_SECRET,
+//   Bucket: process.env.BUCKET_NAME
+// });
 
-  const file = {
-    id,
-    filename,
-    mimetype,
-    encoding,
-    url
-  };
+// const processUpload = async (upload, ctx, info) => {
+//   if (!upload) {
+//     throw new Error("ERROR: No file received.");
+//   }
 
-  console.log("saved prisma file:");
-  console.log(file);
+//   const { stream, filename, mimetype, encoding } = await upload;
+//   const key = uuid() + "-" + filename;
 
-  return file;
-};
+//   // Upload to S3
+//   const response = await s3
+//     .upload({
+//       Key: key,
+//       ACL: "public-read",
+//       Body: stream
+//     })
+//     .promise();
+
+//   const url = response.Location;
+
+//   // Sync with Prisma
+//   const data = {
+//     filename,
+//     mimetype,
+//     encoding,
+//     url
+//   };
+
+//   const file = await ctx.db.mutation.createFile({ data }, info);
+
+//   console.log("saved prisma file:");
+//   console.log(file);
+
+//   return file;
+// };
+
+// export default processUpload;
