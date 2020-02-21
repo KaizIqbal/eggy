f4b3fbcdca7caa7308eb5b8d205c315a8a730065
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-02-21 22:57:43 +0530

    🔒 Add regex in InlineEdit

--- services/client/components/cursor/CursorNameEdit.tsx
+++ services/client/components/cursor/CursorNameEdit.tsx
@@ -11,10 +11,19 @@ const CursorEdit: React.FunctionComponent<ICursorEditProps> = props => {
   // ##### INLINE EDIT HOOKS #####
   // set cursor name default in inline Component
   const [cursorName, setCursorName] = useState(props.name);
+  const regex = /^[A-Za-z]+$/;
+
   // ##### RENDER #####
   return (
     <>
-      <InlineEdit text={cursorName} onSetText={text => setCursorName(text)} />
+      <InlineEdit
+        text={cursorName}
+        regex={regex}
+        onSetText={text =>
+          text.length === 0 ? setCursorName(props.name) : setCursorName(text)
+        }
+        maxLength={12}
+      />
     </>
   );
 };
