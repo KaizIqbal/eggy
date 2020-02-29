5b3e6d872b5cd5375460ce6c06445b7f18d9b64e
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-02-29 21:00:19 +0530

    ➕ Add user Dashboard page based on auth context

--- services/client/components/user/Owner.tsx
+++ services/client/components/user/Owner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import useUser from "../../hooks/graphql/user";
-import PublicPage from "./Public";
+import UserProfile from "./Profile";
 import Head from "next/head";
 
 // ##### COMPONENT PROPS TYPE #####
@@ -24,7 +24,7 @@ const Owner: React.FunctionComponent<IOwnerProps> = props => {
 
   // user not have permission or user not login so render public page
   if (!me || me.username !== props.username) {
-    return <PublicPage username={props.username} />;
+    return <UserProfile username={props.username} />;
   }
 
   // user is Owner so have permission to perform egg operation
