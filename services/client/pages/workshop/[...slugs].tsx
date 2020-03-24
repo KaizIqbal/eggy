ccac407ca9b9dc8eb454f6ee48e7650b65376a5f
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-03-24 15:33:44 +0530

    ➕ Redirection added in Workshop Page

--- services/client/pages/workshop/[...slugs].tsx
+++ services/client/pages/workshop/[...slugs].tsx
@@ -1,36 +1,60 @@
 import React from "react";
-import { NextPage } from "next";
+import fetch from "isomorphic-unfetch";
+import { NextPage, NextPageContext } from "next";
 
 import Page from "components/Page";
 
 import { Redirect } from "lib/redirect";
+import { endpoint } from "lib/endpoint";
 
 interface IProps {
-  slugs?: any;
+  eggname: string;
 }
 
-const Workshop: NextPage<IProps> = ({ slugs }) => {
-  if (!slugs) return <p>Oops</p>;
-
+const Workshop: NextPage<IProps> = ({ eggname }) => {
   return (
     <Page title="Eggy Workshop">
       <h1>Workshop</h1>
-      {slugs.map((slug: any) => (
-        <p key={slug}>{slug}</p>
-      ))}
+      {eggname}
     </Page>
   );
 };
 
-Workshop.getInitialProps = async ctx => {
-  const { query } = ctx;
+Workshop.getInitialProps = async (context: NextPageContext) => {
+  const { query } = context;
 
   const { slugs } = query;
-  if (!slugs || slugs.length > 4) {
-    Redirect(ctx, "/basket");
+
+  // valid url => /workshop/eggname/flavorname/cursorname = 3 slug
+  if (!slugs || slugs.length >= 3) {
+    Redirect(context, "/basket");
+  }
+  const eggname = slugs[0];
+
+  // Graphql Query for checking access
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: `{
+        isEggAccessible(eggname: "${eggname}") {
+          access
+        }
+      }`
+    })
+  }).then(response => response.json());
+
+  const {
+    data: {
+      isEggAccessible: { access }
+    }
+  } = response;
+
+  if (!access) {
+    Redirect(context, "/basket");
   }
 
-  return { slugs };
+  return { eggname };
 };
 
 export default Workshop;
