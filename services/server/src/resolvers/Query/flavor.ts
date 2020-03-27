a2c7a844c53ff14a465adf195ef956ccf8488f10
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-03-27 16:38:55 +0530

    🛠️ Fix Flavor Query

--- services/server/src/resolvers/Query/flavor.ts
+++ services/server/src/resolvers/Query/flavor.ts
@@ -4,18 +4,18 @@ import isAuth from "../../utils/isAuth";
 export const flavorQueries = {
   // ################################################ FOR FETCH SINGLE FLAVOR ################################################
 
-  async flavor(parent, args, ctx, info) {
+  flavor(parent, args, ctx, info) {
     // Checking user logged in or not if not then throw Error
     isAuth(ctx);
 
-    const flavor = await ctx.db.query.flavor({
-      where: {
-        id: args.id
-      }
-    });
-
-    console.log(flavor);
-    return flavor;
+    return ctx.db.query.flavor(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   },
 
   // ################################################ FOR FETCH FLAVORS IN EGG ################################################
