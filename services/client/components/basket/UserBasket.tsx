eb426cd9efac11d39b59adfbd57bce3849583eeb
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-03-29 18:35:21 +0530

    💅 Styled Cursors Cards

--- services/client/components/basket/UserBasket.tsx
+++ services/client/components/basket/UserBasket.tsx
@@ -63,13 +63,13 @@ export const UserBasket: React.FC<IProps> = _props => {
           {egg.title + " "}
           <Button
             onClick={() => {
-              Router.push("/workshop/[slug]", `/workshop/${egg.eggname}`, { shallow: true });
+              Router.push("/workshop/[slug]", `/workshop/${egg.eggname}`);
             }}>
             Workshop
           </Button>
           <Button
             onClick={() => {
-              Router.push("/eggs/[eggname]", `/eggs/${egg.eggname}`, { shallow: true });
+              Router.push("/eggs/[eggname]", `/eggs/${egg.eggname}`);
             }}>
             Page
           </Button>
