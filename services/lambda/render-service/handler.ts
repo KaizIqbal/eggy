8bb24e974a577ac79d64b55937bb57fdc914b83a
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-04-13 08:42:58 +0530

    📦Chromiumm Setup

--- services/lambda/render-service/handler.ts
+++ services/lambda/render-service/handler.ts
@@ -1,12 +1,44 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
+import " source-map-support/register ";
+import { Handler } from "aws-lambda";
+import * as chromium from "chrome-aws-lambda";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const execute: Handler = async (_event, _context) => {
+  let result: any;
+  let browser: any;
+
+  try {
+    // The binary installed by chrome-aws-lambda doesn't work on Windows
+    // When running locally, substitute Chrome which is normally installed
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      ignoreDefaultArgs: process.env.IS_LOCAL ? [" --single-process "] : [],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: process.env.IS_LOCAL
+        ? "/usr/bin/google-chrome-stable"
+        : await chromium.executablePath,
+      headless: false // If you set this to false, the Chrome window will be displayed, so if it does not work during development it is better to change it
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto("https://example.com");
+
+    result = await page.title();
+    console.log(result);
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+    body: JSON.stringify(
+      {
+        message: `Title: $ { result } `
+      },
+      null,
+      2
+    )
   };
-}
+};
