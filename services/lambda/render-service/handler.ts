62a663827bed5c41c54a80ff593f45412ba29297
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-04-17 15:14:45 +0530

    ⚠ cleanup

--- services/lambda/render-service/handler.ts
+++ services/lambda/render-service/handler.ts
@@ -1,18 +1,18 @@
 import { Handler } from "aws-lambda";
 import "source-map-support/register";
-import * as fs from "fs";
-import * as chromium from "chrome-aws-lambda";
-import timesnap from "timesnap";
-import { readFiles } from "./utils/readFiles";
+// import * as chromium from "chrome-aws-lambda";
+// import { renderTemplate } from "./template/render";
 
 // aws-sdk is always preinstalled in AWS Lambda in all Node.js runtimes
 import * as S3Client from "aws-sdk/clients/s3";
 const s3 = new S3Client({ region: process.env.S3_REGION });
 
 export const render: Handler = async (event, _context) => {
+  let browser: any, result: any;
   const srcKey = event.srcKey;
+
   let srcSvg: string;
-  let template: string;
+  // let template = renderTemplate;
 
   // Download the .svg file from the S3 source bucket.
   try {
@@ -20,51 +20,44 @@ export const render: Handler = async (event, _context) => {
       Bucket: process.env.BUCKET_NAME,
       Key: srcKey
     };
-    srcSvg = s3.getObject(params).toString();
+    const response = s3.getObject(params);
+    console.log(response);
+    result = response;
   } catch (error) {
     return;
   }
 
-  template = fs.readFileSync(__dirname + "demo/render.html").toString();
-  template = template.replace("<svginjection>", srcSvg);
-
   try {
-    fs.writeFileSync(__dirname + "index.html", template);
-    //file written successfully
-  } catch (err) {
-    console.error(err);
-    return;
-  }
-
-  await timesnap({
-    executablePath: process.env.IS_LOCAL
-      ? "/usr/bin/google-chrome-stable"
-      : await chromium.executablePath,
-    transparentBackground: true,
-    selector: "svg",
-    fps: 1,
-    duration: 44,
-    outputDirectory: __dirname + "render/raw",
-    outputPattern: __dirname + "animated-%d.png"
-  });
-
-  // use an absolute path to the folder where files are located
-  readFiles(
-    __dirname + "render/raw/",
-    (filename: string, content: Buffer) => {
-      console.log("file name:", filename);
-      console.log(content);
-    },
-    (err: any) => {
-      throw err;
+    // 👇 👇 Injecting .svg file in template here 👇 👇
+    // template = template.replace("<svginjection>", srcSvg);
+    // // The binary installed by chrome-aws-lambda doesn't work on linux
+    // // When running locally, substitute Chrome which is normally installed
+    // browser = await chromium.puppeteer.launch({
+    //   args: chromium.args,
+    //   ignoreDefaultArgs: process.env.IS_LOCAL ? [" --single-process "] : [],
+    //   defaultViewport: chromium.defaultViewport,
+    //   executablePath: process.env.IS_LOCAL
+    //     ? "/usr/bin/google-chrome-stable"
+    //     : await chromium.executablePath,
+    //   headless: chromium.headless // If you set this to false, the Chrome window will be displayed, so change it if it does not work during development.
+    // });
+    // const page = await browser.newPage();
+    // await page.setContent(template);
+    // await page.evaluate(() => (document.body.style.background = "transparent")); // for transparent background
+    // await page.waitForNavigation({ waitUntil: "load" });
+    // const svgElement = await page.select("svg");
+    // result = await svgElement.screenshot({ omitBackground: true });
+  } finally {
+    if (browser) {
+      await browser.close();
     }
-  );
+  }
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "hello"
+        message: result
       },
       null,
       2
