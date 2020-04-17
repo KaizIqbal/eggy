e01d95ee20eea6a1768de4b7180f4a1ee27ab81c
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-04-17 15:14:45 +0530

    ⚠ cleanup

--- services/lambda/render-service/template/render.ts
+++ services/lambda/render-service/template/render.ts
@@ -1,16 +1,18 @@
+export const renderTemplate = `
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Eggy Render Template</title>
         <style>
             svg {width: 50%;height: 50%;}
         </style>
-    </head>
+        </head>
                 
     <body>
         <!-- .svg file injected on <svg> tag -->
         <svg-inject>
     </body>
 </html>
+`;
