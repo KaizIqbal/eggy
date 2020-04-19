bb60340c6c38793fe618a7758906f6965287b6cd
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-04-19 10:57:22 +0530

    ⚡ Lambda cloud execution

--- services/lambda/render-service/handler.ts
+++ services/lambda/render-service/handler.ts
@@ -1,5 +1,6 @@
 import { Handler } from "aws-lambda";
 import "source-map-support/register";
+import * as path from "path";
 
 import { fetchSvgFromS3 } from "./utils/fetchSvgFromS3";
 import { generateRenderTemplate } from "./template/render";
@@ -10,12 +11,16 @@ export const render: Handler = async (_event, _context) => {
   const key: string = "@ful1ie5_/Bibata-KflzAZTR5/Sharp/source/X11.svg";
   const frames: number = 2;
 
-  const { svg, fileName } = await fetchSvgFromS3(key);
+  const svg = await fetchSvgFromS3(key);
+
+  // get fileName from key & remove extension
+  let fileName = path.parse(key).base;
+  fileName = fileName.split(".")[0].toString();
+
   const template = generateRenderTemplate(svg);
-  const renderImages = renderSvg(template, frames, fileName);
+  const renderImages = await renderSvg(template, frames, fileName);
 
   console.log(renderImages);
-
   return {
     statusCode: 200,
     body: JSON.stringify(
