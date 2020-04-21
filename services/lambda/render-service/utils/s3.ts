fb3f868ec857c11f4127a15a7c4febae0b2ca09b
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-04-21 16:44:37 +0530

    🎉 finish

--- services/lambda/render-service/utils/s3.ts
+++ services/lambda/render-service/utils/s3.ts
@@ -3,41 +3,27 @@ import * as S3Client from "aws-sdk/clients/s3";
 const s3 = new S3Client({ region: process.env.S3_REGION });
 
 async function fetchFromS3(key: string) {
-  let response: any;
   // Fetching from S3
-  try {
-    const params = {
-      Bucket: process.env.S3_BUCKET,
-      Key: key
-    };
-    response = await s3.getObject(params).promise();
-  } catch (error) {
-    throw new Error(error);
-  } finally {
-    return response;
-  }
+  const params = {
+    Bucket: process.env.S3_BUCKET,
+    Key: key
+  };
+  return await s3.getObject(params).promise();
 }
 
 async function uploadToS3(key: string, contentType: string, stream: Buffer) {
-  let response: any;
-  try {
-    // Configure parameter
-    const params = {
-      Bucket: process.env.S3_BUCKET,
-      Key: key,
-      ACL: "public-read",
-      ContentType: contentType,
-      Body: stream
-    };
+  // Configure parameter
+  const params = {
+    Bucket: process.env.S3_BUCKET,
+    Key: key,
+    ACL: "public-read",
+    ContentType: contentType,
+    Body: stream
+  };
 
-    // Uploading to S3
-    // TODO : return response.Key
-    response = await s3.upload(params).promise();
-  } catch (error) {
-    throw new Error(error);
-  } finally {
-    return response;
-  }
+  // Uploading to S3
+
+  return await s3.upload(params).promise();
 }
 
 export { fetchFromS3, uploadToS3 };
