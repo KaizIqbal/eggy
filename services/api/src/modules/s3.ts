5f252ec8c4d06dfb6f8c7aff8258d6f10c7b7b1d
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-04-26 13:39:05 +0530

    ⚡ aws configure

--- services/api/src/modules/s3.ts
+++ services/api/src/modules/s3.ts
@@ -1,12 +1,16 @@
-import { aws } from "./awsConfig";
+import * as S3 from "aws-sdk/clients/s3";
 
-const s3bucket = new aws.S3({
-  Bucket: process.env.S3_BUCKET
-});
+let s3Bucket: string;
+if (process.env.S3_BUCKET) {
+  s3Bucket = process.env.S3_BUCKET;
+}
+
+const s3 = new S3();
 
 export function uploadToS3(key: any, mimetype: any, stream: any) {
   // Configure parameter
   const params = {
+    Bucket: s3Bucket,
     Key: key,
     ACL: "public-read",
     ContentType: mimetype,
@@ -14,16 +18,17 @@ export function uploadToS3(key: any, mimetype: any, stream: any) {
   };
 
   // Uploading to S3
-  const s3Response = s3bucket.upload(params).promise();
+  const s3Response = s3.upload(params).promise();
 
   return s3Response;
 }
 
 export function fetchFroms3(key: string) {
   const params = {
+    Bucket: s3Bucket,
     Key: key
   };
-  const s3Response = s3bucket.getObject(params).promise();
+  const s3Response = s3.getObject(params).promise();
 
   return s3Response;
 }
@@ -31,11 +36,12 @@ export function fetchFroms3(key: string) {
 export function deleteFromS3(key: any) {
   // Configure parameter
   const params = {
+    Bucket: s3Bucket,
     Key: key
   };
 
   // Deleting from S3
-  const s3Response = s3bucket.deleteObject(params).promise();
+  const s3Response = s3.deleteObject(params).promise();
 
   return s3Response;
 }
