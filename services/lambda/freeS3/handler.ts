cd5000be2f2220760d553119e221c23eb819f824
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-04-29 16:42:51 +0530

    📦 Package cleanup

--- services/lambda/freeS3/handler.ts
+++ services/lambda/freeS3/handler.ts
@@ -6,9 +6,6 @@ import { generateRenderTemplate } from "./template/render";
 import { fetchFile } from "./utils/fetchFile";
 import { renderSvg } from "./utils/renderSvg";
 
-import { sizes } from "./sizes";
-import { deleteFromS3 } from "./utils/s3";
-
 // This Function for generate png file on s3
 export const render: Handler = async (event, context) => {
   const { srcKey, destKey, frames } = event;
@@ -43,49 +40,5 @@ export const render: Handler = async (event, context) => {
     return context.fail(error);
   } finally {
     return context.succeed(result);
-  }
-};
-
-// trigger when files deleted in "raw" folder
-export const freeS3: Handler = async (event, context) => {
-  // Object key may have spaces or unicode non-ASCII characters.
-  const srcKey = decodeURIComponent(
-    event.Records[0].s3.object.key.replace(/\+/g, " ")
-  );
-
-  // check key is valid
-  if (!srcKey.includes("raw/")) {
-    console.log("No Action!!! Operation is not in 'raw' directory");
-    return;
-  }
-  // Infer the image type from the file suffix.
-  const typeMatch = srcKey.match(/\.([^.]*)$/);
-  if (!typeMatch) {
-    console.log("No Action!! Could not determine the image type.");
-    return;
-  }
-
-  // Check that the image type is supported
-  const imageType = typeMatch[1].toLowerCase();
-  if (imageType !== "png") {
-    console.log(`No Action!! Unsupported image type: ${imageType}`);
-    return;
-  }
-
-  // generating other related keys
-  const keys: array<string> = [];
-
-  sizes.forEach(size => {
-    keys.push(srcKey.replace("raw", `${size}x${size}`));
-  });
-
-  try {
-    keys.forEach(key => {
-      deleteFromS3(key);
-    });
-  } catch (error) {
-    return context.fail(error);
-  } finally {
-    return context.succeed(null);
   }
 };
