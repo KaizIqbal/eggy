6061c1a4aa3ab6dc146194e3fb7bdd3e3cdfd201
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-04-30 17:23:04 +0530

    💻 Dev Branch

--- services/api/src/resolvers/Mutation/file.ts
+++ services/api/src/resolvers/Mutation/file.ts
@@ -1,4 +1,4 @@
-import { uploadToS3, deleteFromS3 } from "../../modules/s3";
+import { uploadToS3, deleteFromS3, deleteRenderImages } from "../../modules/s3";
 import isAuth from "../../utils/isAuth";
 
 export const fileMutations = {
@@ -136,13 +136,7 @@ export const fileMutations = {
     });
 
     if (count > 0) {
-      const keys: array<string> = [];
-      const responseKeys = JSON.parse(JSON.stringify(data.cursor.render));
-
-      responseKeys.filter(obj => keys.push(obj.key));
-
-      // Deleting render files from S3
-      keys.forEach(key => deleteFromS3(key));
+      deleteRenderImages(data.cursor.render);
     }
 
     await ctx.db.mutation.updateCursor({
---
+++ services/lambda/render/handler.ts
@@ -0,0 +1,44 @@
+import { Handler } from "aws-lambda";
+import "source-map-support/register";
+import * as path from "path";
+
+import { generateRenderTemplate } from "./template/render";
+import { fetchFile } from "./utils/fetchFile";
+import { renderSvg } from "./utils/renderSvg";
+
+// This Function for generate png file on s3
+export const render: Handler = async (event, context) => {
+  const { srcKey, destKey, frames } = event;
+  let result: any;
+
+  if (!srcKey || !destKey || !frames) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          error: "Some Arguments are missing"
+        },
+        null,
+        2
+      )
+    };
+  }
+
+  try {
+    // fix destKey :: store rendered images in directory not in file
+    const destPath = destKey.endsWith("/") ? destKey : destKey.concat("/");
+
+    const svg = await fetchFile(srcKey);
+
+    // get fileName from key & remove extension
+    let fileName = path.parse(srcKey).base;
+    fileName = fileName.split(".")[0];
+
+    const template = generateRenderTemplate(svg);
+    result = await renderSvg(template, frames, fileName, destPath);
+  } catch (error) {
+    return context.fail(error);
+  } finally {
+    return context.succeed(result);
+  }
+};
