6a1dad9ee60d2f6f2b84217ef5c6149fac6f3a18
K a i z <24286590+KaizIqbal@users.noreply.github.com>
2020-05-04 17:13:56 +0530

    😧 Publish flavor

--- services/app/components/flavor/index.ts
+++ services/app/components/flavor/index.ts
@@ -1,6 +1,7 @@
 import { CreateFlavor } from "./Create";
 import { RenameFlavor } from "./Rename";
 import { DeleteFlavor } from "./Delete";
+import { PublishFlavor } from "./Publish";
 import { Flavors } from "./Flavors";
 
-export { CreateFlavor, RenameFlavor, DeleteFlavor, Flavors };
+export { CreateFlavor, RenameFlavor, DeleteFlavor, PublishFlavor, Flavors };
