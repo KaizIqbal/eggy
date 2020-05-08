4ab5c75bb8b8e655cd88c9d926a325a08976a40d
K A ! Z <kaiz@pop-os.localdomain>
2020-05-08 16:43:51 +0530

    ⚡ Bundle Lambda Function Boilerplate

---
+++ lambda/bundle/handler.ts
@@ -0,0 +1,17 @@
+import { Handler } from "aws-lambda";
+import "source-map-support/register";
+
+
+// This Function for generate png file on s3
+export const render: Handler = async (_event, context) => {
+
+  let bundle: any;
+
+  try {
+    bundle = "This is a Bundle"
+  } catch (error) {
+    return context.fail(error);
+  } finally {
+    return context.succeed(bundle);
+  }
+};
