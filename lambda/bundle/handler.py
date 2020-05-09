23bd671b5b3c9d8a85af5e62883f8d33be7ddfe1
K A ! Z <kaiz@pop-os.localdomain>
2020-05-09 10:19:53 +0530

    ⚡ Local execution

--- lambda/bundle/handler.py
+++ lambda/bundle/handler.py
@@ -2,16 +2,20 @@ import json
 import os
 
 anicursorgen = "./tools/anicursorgen.py"
+xcursorgen = "./tools/xcursorgen"
 
 
 def bundle(event, context):
 
-    # output = os.system("%s -h" % anicursorgen)
-    os.environ["S3_BUCKET"]
+    win = os.system("%s -h" % anicursorgen)
+    x11 = os.system("%s --help" % xcursorgen)
+    env = os.environ["S3_BUCKET"]
 
     response = {
         "statusCode": 200,
-        # "bundle": json.dumps(output)
+        "win": json.dumps(win),
+        "x11": json.dumps(x11),
+        "env": json.dumps(env)
     }
 
     return response
