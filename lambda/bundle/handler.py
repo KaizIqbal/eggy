a777c04bf95e5fd09b189f4ec1b9d84df2e66ced
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-09 17:28:30 +0530

    ⚡ execute tool in handler

--- lambda/bundle/handler.py
+++ lambda/bundle/handler.py
@@ -1,21 +1,16 @@
 import json
 import os
 
-anicursorgen = "./tools/anicursorgen.py"
-xcursorgen = "./tools/xcursorgen"
-
 
 def bundle(event, context):
 
-    win = os.system("%s -h" % anicursorgen)
-    x11 = os.system("%s --help" % xcursorgen)
-    env = os.environ["S3_BUCKET"]
+    x11 = os.system("./xcursorgen --help")
+    print(x11)
+
+    # env = os.getenv("S3_BUCKET")
 
     response = {
         "statusCode": 200,
-        "win": json.dumps(win),
-        "x11": json.dumps(x11),
-        "env": json.dumps(env)
     }
 
     return response
