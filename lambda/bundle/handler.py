6d453d48023f4df761e5677470c51be26f0b34e2
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-11 14:39:36 +0530

    ⚡ handler updated

--- lambda/bundle/handler.py
+++ lambda/bundle/handler.py
@@ -1,24 +1,7 @@
-import boto3
 import json
-import os
-import uuid
 
-region = os.environ.get("REGION")
-bucket = os.environ.get("S3_BUCKET")
-
-
-def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
-    s3_resource = boto3.resource('s3', region)
-    bucket = s3_resource.Bucket(bucketName)
-    for object in bucket.objects.filter(Prefix=remoteDirectoryName):
-        if not os.path.exists(os.path.dirname(object.key)):
-            os.makedirs(os.path.dirname(object.key))
-        bucket.download_file(object.key, object.key)
-
-
-def generate_x11():
-    os.environ["LD_LIBRARY_PATH"] = os.getcwd() + "/lib"
-    print(os.system("./xcursorgen --help"))
+# modules
+from bundler import fetch, ini
 
 
 def bundle(event, context):
@@ -26,7 +9,11 @@ def bundle(event, context):
     key = event["key"]
     # type = event["type"]
 
-    downloadDirectoryFroms3(bucket, key)
+    print("🚛 Fetching resources from S3...")
+    fetch.directory_from_s3(key)
+
+    print("🔥 Generating Configs...")
+    ini.generate("/tmp/test")
 
     response = {
         "statusCode": 200,
