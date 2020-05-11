c9e81499ab651651f013e003d2d81f5d1f96f48a
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-11 14:38:09 +0530

    ⚡ fetch sub_module added

---
+++ lambda/bundle/bundler/fetch.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+import uuid
+
+region = os.environ.get("REGION")
+s3_resource = boto3.resource('s3', region)
+bucket = s3_resource.Bucket(os.environ.get("S3_BUCKET"))
+
+# generate 8 character long unique id
+uid = str(uuid.uuid4())[:8]
+
+
+def directory_from_s3(s3_dir):
+    """ Fetch directory from S3Bucket,Config fetched from environment variables.
+        To set manually try commands ```
+            export REGION=<your-region>
+            export S3_BUCKET=<your-s3-bucket-name>
+        ``` or create '.env' file that automatically detect by serverless
+    """
+
+    for object in bucket.objects.filter(Prefix=s3_dir):
+        path = "%s/%s" % (uid, object.key.replace(s3_dir, ""))
+        if not os.path.exists(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path))
+        bucket.download_file(object.key, path)
