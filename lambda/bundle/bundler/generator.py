4ca9e86d0064996b7114662e2a114f4de6bb6075
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-11 18:37:58 +0530

    📔 TODO added

--- lambda/bundle/bundler/generator.py
+++ lambda/bundle/bundler/generator.py
@@ -5,13 +5,16 @@ def x11():
     print("Generating X11 Cursors...")
     # setup environment
     os.environ["LD_LIBRARY_PATH"] = os.getcwd() + "/lib"
+    #  TODO
     print(os.system("./xcursorgen --help"))
 
 
 def window():
+    # TODO
     print("Generating Window Cursors..")
 
 
 def all():
+    # TODO
     print("Generating Window Cursors")
     print("Generating X11 Cursors")
