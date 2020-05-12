816b5d019badc2feaba1a4875852068422a1008f
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-12 18:55:08 +0530

    🗃 Analyse all cusror

--- lambda/bundle/bundler/ini.py
+++ lambda/bundle/bundler/ini.py
@@ -1,7 +1,10 @@
+import os
+# modules
 from . import config
 
 
 def write_xcur(imgs_dir=config.WORK_DIR, cursor_type=config.CURSOR_TYPE, cursor_dpi=config.SIZES):
     """ Generate .ini config files."""
-    for dpi in cursor_dpi:
-        print(dpi)
+    raw_dir = config.RAW_DIR
+    for filename in os.listdir(raw_dir):
+        print(filename)
