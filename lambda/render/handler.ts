10963cbaa3f705189e3a053dca1b1729ffb95745
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-14 09:49:47 +0530

    ⚡ changed

--- lambda/render/handler.ts
+++ lambda/render/handler.ts
@@ -16,11 +16,11 @@ export const render: Handler = async (event, context) => {
       statusCode: 500,
       body: JSON.stringify(
         {
-          error: "Some Arguments are missing"
+          error: "Some Arguments are missing",
         },
         null,
         2
-      )
+      ),
     };
   }
 
@@ -32,10 +32,11 @@ export const render: Handler = async (event, context) => {
 
     // get fileName from key & remove extension
     let fileName = path.parse(srcKey).base;
-    fileName = fileName.split(".")[0];
+    fileName = fileName.split(".")[0].toLowerCase();
 
     const template = generateRenderTemplate(svg);
-    result = await renderSvg(template, frames, fileName, destPath);
+
+    result = renderSvg(template, frames, fileName, destPath);
   } catch (error) {
     return context.fail(error);
   } finally {
