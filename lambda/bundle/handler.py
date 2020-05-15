fcb8014d58c76a555b710678f3f43a3bf7701e0c
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-15 10:19:03 +0530

    🗒 var set to handler

--- lambda/bundle/handler.py
+++ lambda/bundle/handler.py
@@ -14,16 +14,15 @@ def bundle(event, context):
     dir = str(uuid.uuid4())[:8]
 
     # configs
-    bundler.config.CURSOR_TYPE = event["type"]
-    bundler.config.DPI = event["sizes"]
-    bundler.config.WORK_DIR = dir
-    bundler.config.RAW_DIR = dir + "/raw/"
+    bundler.var.CURSOR_TYPE = event["type"]
+    bundler.var.DPI = event["sizes"]
+    bundler.var.WORK_DIR = dir
 
     print("🚛 Fetching resources from S3...")
     fetch.directory_from_s3(s3_dir=key, local_dir=dir)
 
-    print("🔥 Generating config files...")
-    bundler.generate_ini()
+    print("🔥 Generating helper files...")
+    bundler.generate_helper()
 
     print("📦 Creating bundle...")
     bundle = bundler.create_bundle()
