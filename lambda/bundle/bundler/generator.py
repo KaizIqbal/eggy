2054ca174436288f0259655ace7a69b49ba8862f
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-15 12:41:23 +0530

    🗒 config generator added

--- lambda/bundle/bundler/generator.py
+++ lambda/bundle/bundler/generator.py
@@ -1,4 +1,13 @@
 import os
+import glob
+
+
+def generate_sizes(imgs_dir, cursor_sizes):
+    pass
+
+
+def generate_xcur_config(imgs_dir, cursor_sizes):
+    pass
 
 
 def x11():
@@ -18,3 +27,16 @@ def all():
     # TODO
     print("Generating Window Cursors")
     print("Generating X11 Cursors")
+
+
+def generate_helper(imgs_dir, cursor_sizes):
+    """ Generate helpers files."""
+    # for statis files
+    for file_path in glob.glob("%s/*[!0-9].*" % imgs_dir):
+        file_name = os.path.basename(file_path)
+        print(file_name)
+
+    # for animated files
+    for file_path in glob.glob("%s/*[0-9].*" % imgs_dir):
+        file_name = os.path.basename(file_path)
+        print(file_name)
