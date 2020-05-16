4980e2089bbc9b98332412f69d4f9d294d2d16d3
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-16 08:43:02 +0530

    📜 static and animated list generated

--- lambda/bundle/bundler/helpers.py
+++ lambda/bundle/bundler/helpers.py
@@ -1,17 +1,24 @@
 import os
+import itertools
 
 
 def get_cursor_list(imgs_dir, animated=False):
-    all_curosr_list, list = [], []
+    all_curosr_list, cursor_list = [], []
 
     for file_path in os.listdir(imgs_dir):
         all_curosr_list.append(os.path.basename(file_path))
 
     if (animated):
         # animated cursor have filename-1,2,3..n postfix
-        list = [i for i in all_curosr_list if str(i).find("-") >= 0]
+        temp = [cursor for cursor in all_curosr_list if
+                cursor.find("-") >= 0]
+        temp.sort()
+        cursor_list = [list(g) for _, g in itertools.groupby(
+            temp, lambda x: x.partition("-")[0])]
     else:
-        list = [i for i in all_curosr_list if str(i).find("-") <= 0]
+        for cursor in all_curosr_list:
+            if cursor.find("-") <= 0:
+                cursor_list.append(cursor)
+        cursor_list.sort()
 
-    list.sort()
-    return list
+    return cursor_list
