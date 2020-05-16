07b0cd7e781707a0ee7aecd4b5896f216e00d9d0
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-16 10:02:24 +0530

    📸 render sizes

--- lambda/bundle/bundler/generator.py
+++ lambda/bundle/bundler/generator.py
@@ -1,15 +1,16 @@
 import os
 import glob
+from PIL import Image
 
 # modules
 from . import helpers
 
 
-def generate_sizes(imgs_dir, cursor_sizes):
+def generate_sizes(cursors_list, cursor_sizes):
     pass
 
 
-def generate_xcur_config(imgs_dir, cursor_sizes):
+def generate_xcur_config(cursors_list, cursor_sizes):
     pass
 
 
@@ -34,5 +35,22 @@ def all():
 
 def generate_helper(imgs_dir, cursor_sizes):
     """ Generate helpers files."""
-    static_cursors = helpers.get_cursor_list(imgs_dir)
-    animated_cursors = helpers.get_cursor_list(imgs_dir, animated=True)
+    # Configure static cursors
+    static_cursors_list = helpers.get_cursor_list(imgs_dir)
+
+    for cursor in static_cursors_list:
+        for size in cursor_sizes:
+            # helper variables
+            sizes = (size, size)
+            in_path = imgs_dir + "/" + cursor
+            out_dir = imgs_dir + "/%sx%s/" % (size, size)
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+
+            # resizing & save
+            im = Image.open(in_path)
+            resized_im = im.resize(sizes, Image.ANTIALIAS)
+            resized_im.save(out_dir+cursor)
+
+    # configure animated cursors
+    animated_cursors_groups = helpers.get_cursor_list(imgs_dir, animated=True)
