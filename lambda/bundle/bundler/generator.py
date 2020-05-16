4df1fb26cc606412a918d33e83d3d37b4e57c80e
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-16 17:06:33 +0530

    💡 Config generator in helpers

--- lambda/bundle/bundler/generator.py
+++ lambda/bundle/bundler/generator.py
@@ -1,6 +1,5 @@
 import os
 import glob
-from PIL import Image
 
 # modules
 from . import helpers
@@ -28,35 +27,8 @@ def all():
 def generate_helper(imgs_dir, cursor_sizes):
     """ Generate helpers files."""
     # Configure static cursors
-    static_cursors_list = helpers.get_cursor_list(imgs_dir)
-
-    for cursor in static_cursors_list:
-        config_file_path = imgs_dir+"/"+cursor.replace(".png", ".in")
-        config_file = open(config_file_path, "w")
-
-        for size in cursor_sizes:
-            # helper variables
-            sizes = (size, size)
-            in_path = imgs_dir + "/" + cursor
-            out_dir = imgs_dir + "/%sx%s/" % (size, size)
-            out_path = out_dir+cursor
-            if not os.path.exists(out_dir):
-                os.makedirs(out_dir)
-
-            # resizing & save
-            im = Image.open(in_path)
-            resized_im = im.resize(sizes, Image.ANTIALIAS)
-            resized_im.save(out_path)
-
-            # config file content
-            # add new line on every line but not on last line
-            line = "%s xhot yhot %s\n" % (size, out_path)
-            if size == cursor_sizes[-1]:
-                line = "%s xhot yhot %s" % (size, out_path)
-
-            config_file.write(line)
-
-        config_file.close()
+    static_list = helpers.get_cursor_list(imgs_dir)
+    helpers.static_cursor_config(static_list, imgs_dir, cursor_sizes)
 
     # configure animated cursors
     animated_cursors_groups = helpers.get_cursor_list(imgs_dir, animated=True)
--- lambda/bundle/bundler/helpers.py
+++ lambda/bundle/bundler/helpers.py
@@ -1,5 +1,6 @@
 import os
 import itertools
+from PIL import Image
 
 
 def sort_cursors(cursor_list):
@@ -27,3 +28,33 @@ def get_cursor_list(imgs_dir, animated=False):
         cursor_list.sort()
 
     return cursor_list
+
+
+def static_cursor_config(list, imgs_dir, sizes):
+    for cursor in list:
+        config_file_path = imgs_dir+"/"+cursor.replace(".png", ".in")
+        config_file = open(config_file_path, "w")
+
+        for size in sizes:
+            # helper variables
+            sizes = (size, size)
+            in_path = imgs_dir + "/" + cursor
+            out_dir = imgs_dir + "/%sx%s/" % (size, size)
+            out_path = out_dir+cursor
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+
+            # resizing & save
+            im = Image.open(in_path)
+            resized_im = im.resize(sizes, Image.ANTIALIAS)
+            resized_im.save(out_path)
+
+            # config file content
+            # add new line on every line but not on last line
+            line = "%s xhot yhot %s\n" % (size, out_path)
+            if size == sizes[-1]:
+                line = "%s xhot yhot %s" % (size, out_path)
+
+            config_file.write(line)
+
+        config_file.close()
