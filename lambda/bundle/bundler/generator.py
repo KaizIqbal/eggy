bc783a023330a930ba142dcb3d4b26852fbb70b7
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-16 16:57:38 +0530

    🔧 config files generated for static file

--- lambda/bundle/bundler/generator.py
+++ lambda/bundle/bundler/generator.py
@@ -6,14 +6,6 @@ from PIL import Image
 from . import helpers
 
 
-def generate_sizes(cursors_list, cursor_sizes):
-    pass
-
-
-def generate_xcur_config(cursors_list, cursor_sizes):
-    pass
-
-
 def x11():
     print("Generating X11 Cursors...")
     # setup environment
@@ -39,18 +31,32 @@ def generate_helper(imgs_dir, cursor_sizes):
     static_cursors_list = helpers.get_cursor_list(imgs_dir)
 
     for cursor in static_cursors_list:
+        config_file_path = imgs_dir+"/"+cursor.replace(".png", ".in")
+        config_file = open(config_file_path, "w")
+
         for size in cursor_sizes:
             # helper variables
             sizes = (size, size)
             in_path = imgs_dir + "/" + cursor
             out_dir = imgs_dir + "/%sx%s/" % (size, size)
+            out_path = out_dir+cursor
             if not os.path.exists(out_dir):
                 os.makedirs(out_dir)
 
             # resizing & save
             im = Image.open(in_path)
             resized_im = im.resize(sizes, Image.ANTIALIAS)
-            resized_im.save(out_dir+cursor)
+            resized_im.save(out_path)
+
+            # config file content
+            # add new line on every line but not on last line
+            line = "%s xhot yhot %s\n" % (size, out_path)
+            if size == cursor_sizes[-1]:
+                line = "%s xhot yhot %s" % (size, out_path)
+
+            config_file.write(line)
+
+        config_file.close()
 
     # configure animated cursors
     animated_cursors_groups = helpers.get_cursor_list(imgs_dir, animated=True)
