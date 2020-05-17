2e5880c8485150afb9fd5c05364591eb2d663757
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-17 10:30:21 +0530

    📸 resize images by size ratio

--- lambda/bundle/bundler/helpers.py
+++ lambda/bundle/bundler/helpers.py
@@ -30,6 +30,44 @@ def get_cursor_list(imgs_dir, animated=False):
     return cursor_list
 
 
+def resize_cursor(cursor, size, imgs_dir):
+    # helper variables
+    in_path = imgs_dir + "/" + cursor
+    out_dir = imgs_dir + "/%sx%s/" % (size, size)
+    out_path = out_dir+cursor
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    # resizing & save
+    image = Image.open(in_path)
+
+    width = image.size[0]
+    height = image.size[1]
+
+    aspect = width / float(height)
+
+    ideal_width = size
+    ideal_height = size
+
+    ideal_aspect = ideal_width / float(ideal_height)
+
+    if aspect > ideal_aspect:
+        # Then crop the left and right edges:
+        new_width = int(ideal_aspect * height)
+        offset = (width - new_width) / 2
+        resize = (offset, 0, width - offset, height)
+    else:
+        # ... crop the top and bottom:
+        new_height = int(width / ideal_aspect)
+        offset = (height - new_height) / 2
+        resize = (0, offset, width, height - offset)
+
+    thumb = image.crop(resize).resize(
+        (ideal_width, ideal_height), Image.ANTIALIAS)
+    thumb.save(out_path)
+
+
 def static_cursor_config(list, imgs_dir, sizes):
     for cursor in list:
         config_file_path = imgs_dir+"/"+cursor.replace(".png", ".in")
@@ -37,22 +75,8 @@ def static_cursor_config(list, imgs_dir, sizes):
 
         sizes_len = len(sizes) - 1
         for index, size in enumerate(sizes):
-            # helper variables
-
-            sizes = (size, size)
-            in_path = imgs_dir + "/" + cursor
-            out_dir = imgs_dir + "/%sx%s/" % (size, size)
-            out_path = out_dir+cursor
-            if not os.path.exists(out_dir):
-                os.makedirs(out_dir)
-
-            # resizing & save
-            im = Image.open(in_path)
-            resized_im = im.resize(sizes, Image.ANTIALIAS)
-            resized_im.save(out_path)
-
+            resize_cursor(cursor, size, imgs_dir)
             # config file content
-            # add new line on every line but not on last line
             line = "%s xhot yhot %sx%s/%s\n" % (size, size, size, cursor)
             if (index == sizes_len):
                 line = "%s xhot yhot %sx%s/%s" % (size, size, size, cursor)
