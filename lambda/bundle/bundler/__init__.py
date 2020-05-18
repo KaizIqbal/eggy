d17023471938411e1915379fed2d917e8078bf28
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-18 16:31:21 +0530

    💡 call tools by output referance

--- lambda/bundle/bundler/__init__.py
+++ lambda/bundle/bundler/__init__.py
@@ -1,20 +1,18 @@
 import os
 
-# modules
-from . import generator
-from . import tools
+from . import generator, tools, template
 
 
 def create_bundle(imgs_dir, cursor_type, cursor_sizes):
     """ Create Bundle from raw images """
     generator.generate_config(imgs_dir, cursor_sizes)
+    ref = template.generate_x11_template("abcd")
 
     if cursor_type == "LINUX":
-        tools.generate_x11()
+        tools.generate_x11(ref)
     elif cursor_type == "WINDOW":
-        tools.generate_window()
+        tools.generate_window(ref)
     else:
-        tools.generate_x11()
-        tools.generate_window()
-
+        tools.generate_x11(ref)
+        tools.generate_window(ref)
     return "bundle"
--- lambda/bundle/bundler/tools/__init__.py
+++ lambda/bundle/bundler/tools/__init__.py
@@ -4,17 +4,16 @@ tools_path = os.path.dirname(__file__)
 xcursorgen = tools_path + "/xcursorgen.out"
 
 
-def generate_x11():
+def generate_x11(to):
     print("Generating X11 Cursors...")
 
     # setup environment
     # os.environ["LD_LIBRARY_PATH"] += tools_path + "/lib"
 
     # generating X11 cursors
-
     os.system("%s --help" % xcursorgen)
 
 
-def generate_window():
+def generate_window(to):
     # TODO
     print("Generating Window Cursors..")
