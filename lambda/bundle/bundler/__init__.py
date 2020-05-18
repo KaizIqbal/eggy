6adc617c4a579df62260f6ad27d72dadba675040
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-18 16:58:57 +0530

    ♻ ref added to tools

--- lambda/bundle/bundler/__init__.py
+++ lambda/bundle/bundler/__init__.py
@@ -6,13 +6,13 @@ from . import generator, tools, template
 def create_bundle(imgs_dir, cursor_type, cursor_sizes):
     """ Create Bundle from raw images """
     generator.generate_config(imgs_dir, cursor_sizes)
-    ref = template.generate_x11_template("abcd")
+    to = template.generate_x11_template("abcd")
 
     if cursor_type == "LINUX":
-        tools.generate_x11(ref)
+        tools.generate_x11(imgs_dir, to)
     elif cursor_type == "WINDOW":
-        tools.generate_window(ref)
+        tools.generate_window(imgs_dir, to)
     else:
-        tools.generate_x11(ref)
-        tools.generate_window(ref)
+        tools.generate_x11(imgs_dir, to)
+        tools.generate_window(imgs_dir, to)
     return "bundle"
