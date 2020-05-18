9c9714707e390332dfcd238cfde9c2075eeaa32d
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-18 09:32:16 +0530

    🧪 test tools

--- lambda/bundle/bundler/tools/__init__.py
+++ lambda/bundle/bundler/tools/__init__.py
@@ -1,12 +1,18 @@
 import os
 
+tools_path = os.path.dirname(__file__)
+xcursorgen = tools_path + "/xcursorgen.out"
+
 
 def generate_x11():
     print("Generating X11 Cursors...")
+
     # setup environment
-    os.environ["LD_LIBRARY_PATH"] = os.getcwd() + "/lib"
-    #  TODO
-    print(os.system("./xcursorgen --help"))
+    # os.environ["LD_LIBRARY_PATH"] += tools_path + "/lib"
+
+    # generating X11 cursors
+
+    os.system("%s --help" % xcursorgen)
 
 
 def generate_window():
