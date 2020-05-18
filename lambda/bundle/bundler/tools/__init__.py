a803c9727c8c2702f5aeb32e0910345a759dd4a0
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-18 17:00:05 +0530

    👷 X11 Cursor build script added

--- lambda/bundle/bundler/tools/__init__.py
+++ lambda/bundle/bundler/tools/__init__.py
@@ -4,16 +4,21 @@ tools_path = os.path.dirname(__file__)
 xcursorgen = tools_path + "/xcursorgen.out"
 
 
-def generate_x11(to):
+def generate_x11(config_dir, to):
     print("Generating X11 Cursors...")
 
     # setup environment
     # os.environ["LD_LIBRARY_PATH"] += tools_path + "/lib"
 
     # generating X11 cursors
-    os.system("%s --help" % xcursorgen)
+    for root, _, files in os.walk(config_dir):
+        for file in files:
+            cursor_name = file.replace(".in", "")
+            if file.endswith(".in"):
+                os.system("%s %s %s" %
+                          (xcursorgen, os.path.join(root, file), to + "/" + cursor_name))
 
 
-def generate_window(to):
+def generate_window(config_dir, to):
     # TODO
     print("Generating Window Cursors..")
