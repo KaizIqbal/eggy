d7c3ce2c31e68e21bb5ee8da42111b18f9a44c2e
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-19 18:47:32 +0530

    👷 Window cursorgen added with helper

--- lambda/bundle/bundler/tools/__init__.py
+++ lambda/bundle/bundler/tools/__init__.py
@@ -2,6 +2,19 @@ import os
 
 tools_path = os.path.dirname(__file__)
 xcursorgen = tools_path + "/xcursorgen.out"
+anicursorgen = tools_path + "/anicursorgen.py"
+
+
+def get_cursor_name(path):
+    with open(path, "r") as config:
+        line = config.readline()
+        words = line.split(" ")
+
+        if(len(words) >= 4):
+            return os.path.basename(path).replace(".in", ".ani")
+        else:
+            return os.path.basename(path).replace(".in", ".cur")
+    config.close()
 
 
 def generate_x11(config_dir, to):
@@ -21,10 +34,11 @@ def generate_x11(config_dir, to):
 
 def generate_window(config_dir, to):
     print("Generating Window Cursors..")
-
-    # generating X11 cursors
     for root, _, files in os.walk(config_dir):
         for file in files:
-            cursor_name = file.replace(".in", "")
+
             if file.endswith(".in"):
-                print(os.path.join(root, file))
+                path = os.path.join(root, file)
+                cursor_name = get_cursor_name(path)
+                os.system("%s %s %s" %
+                          (anicursorgen, path, to + "/" + cursor_name))
