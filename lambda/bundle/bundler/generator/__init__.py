ba59385a3b907c891dd88aaacbaa96ba12ca7ef4
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-20 10:13:56 +0530

    🛣 Absolute path added

--- lambda/bundle/bundler/generator/__init__.py
+++ lambda/bundle/bundler/generator/__init__.py
@@ -8,3 +8,5 @@ def generate_config(imgs_dir, cursor_sizes):
     """ Generate helpers files."""
     helpers.generate_static_cursor(imgs_dir, cursor_sizes)
     helpers.generate_animated_cursor(imgs_dir, cursor_sizes)
+
+    return (os.path.abspath(imgs_dir))
