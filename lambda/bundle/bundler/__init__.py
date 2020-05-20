867fb042191705139da0b32ef484e9ce9bdc8924
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-20 09:46:13 +0530

    📦 create windows cursors

--- lambda/bundle/bundler/__init__.py
+++ lambda/bundle/bundler/__init__.py
@@ -5,14 +5,18 @@ from . import generator, tools, template
 
 def create_bundle(imgs_dir, cursor_type, cursor_sizes):
     """ Create Bundle from raw images """
-    generator.generate_config(imgs_dir, cursor_sizes)
-    to = template.generate_x11_template("abcd")
+    config_dir = generator.generate_config(imgs_dir, cursor_sizes)
+
+    bundle = "bundle"
 
     if cursor_type == "LINUX":
-        tools.generate_x11(imgs_dir, to)
+        to = template.generate_x11_template("abcd")
+        tools.generate_x11(config_dir, to)
     elif cursor_type == "WINDOW":
-        tools.generate_window(imgs_dir, to)
+        to = template.generate_win_template("abcd")
+        tools.generate_window(config_dir, to)
     else:
         tools.generate_x11(imgs_dir, to)
         tools.generate_window(imgs_dir, to)
-    return "bundle"
+
+    return bundle
