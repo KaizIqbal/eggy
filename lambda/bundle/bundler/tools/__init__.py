ff84ef0fd1f7f46439e045c62683af603140c1be
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-05-20 09:46:35 +0530

    📦 compress cursor file

--- lambda/bundle/bundler/tools/__init__.py
+++ lambda/bundle/bundler/tools/__init__.py
@@ -1,20 +1,39 @@
 import os
+import shutil
+import zipfile
+from contextlib import contextmanager
 
 tools_path = os.path.dirname(__file__)
 xcursorgen = tools_path + "/xcursorgen.out"
-anicursorgen = tools_path + "/anicursorgen.py"
+anicursorgen = "." + tools_path + "/anicursorgen.py"
+
+
+@contextmanager
+def cwd(path):
+    oldpwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
+
+
+def create_archive(direcctory):
+    zip_path = shutil.make_archive(direcctory, 'zip', direcctory)
+    out = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED)
+    print(out)
 
 
 def get_cursor_name(path):
     with open(path, "r") as config:
         line = config.readline()
         words = line.split(" ")
-
-        if(len(words) >= 4):
-            return os.path.basename(path).replace(".in", ".ani")
-        else:
-            return os.path.basename(path).replace(".in", ".cur")
     config.close()
+
+    if(len(words) >= 4):
+        return os.path.basename(path).replace(".in", ".ani")
+    else:
+        return os.path.basename(path).replace(".in", ".cur")
 
 
 def generate_x11(config_dir, to):
@@ -24,21 +43,22 @@ def generate_x11(config_dir, to):
     # os.environ["LD_LIBRARY_PATH"] += tools_path + "/lib"
 
     # generating X11 cursors
-    for root, _, files in os.walk(config_dir):
+    for _, _, files in os.walk(config_dir):
         for file in files:
             cursor_name = file.replace(".in", "")
             if file.endswith(".in"):
-                os.system("%s %s %s" %
-                          (xcursorgen, os.path.join(root, file), to + "/" + cursor_name))
+                os.system("%s %s %s" % (xcursorgen, os.path.join(
+                    config_dir, file), to + "/" + cursor_name))
 
 
 def generate_window(config_dir, to):
     print("Generating Window Cursors..")
-    for root, _, files in os.walk(config_dir):
+    for _, _, files in os.walk(config_dir):
         for file in files:
-
             if file.endswith(".in"):
-                path = os.path.join(root, file)
+                path = os.path.join(config_dir, file)
                 cursor_name = get_cursor_name(path)
-                os.system("%s %s %s" %
-                          (anicursorgen, path, to + "/" + cursor_name))
+                with cwd(config_dir):
+                    os.system("%s %s %s" %
+                              (anicursorgen, path, "../" + to + cursor_name))
+    create_archive(to)
