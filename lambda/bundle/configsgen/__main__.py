b4b0eac6c739380cd62abe13878611f7aae6d91f
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-07 16:38:17 +0530

    👷 config builder added

--- lambda/bundle/configsgen/__main__.py
+++ lambda/bundle/configsgen/__main__.py
@@ -1,11 +1,14 @@
 
 import os
+import typing
 
+# types
+from .types import List, Num
 # modules
 from . import helpers
 
 
-def generate_config(imgs_dir, cursor_sizes):
+def generate_config(imgs_dir: str, cursor_sizes: List[Num]):
     """ Generate helpers files."""
     helpers.generate_static_cursor(imgs_dir, cursor_sizes)
     helpers.generate_animated_cursor(imgs_dir, cursor_sizes)
---
+++ lambda/bundle/configsgen/types.py
@@ -0,0 +1,3 @@
+from typing import Union, List
+
+Num = Union[int]
