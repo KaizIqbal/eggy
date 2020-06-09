3138bcf178ac5821a2c8dacef255ff62cb1ca855
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-09 17:35:57 +0530

    🎆 generate cursor package based on clickgen

--- lambda/bundle/handler.py
+++ lambda/bundle/handler.py
@@ -2,7 +2,7 @@ import json
 import os
 import tempfile
 import shutil
-
+import clickgen
 
 # modules
 import configsgen
@@ -15,16 +15,28 @@ def bundle(event, context):
     type = event["type"]
     sizes = event["sizes"]
 
-    dir = tempfile.mkdtemp()
+    imgs_dir = tempfile.mkdtemp()
 
-    print("🚛 Fetching resources from S3 to %s ..." % dir)
-    s3.fetch_directory(s3_dir=key, local_dir=dir)
+    print("🚛 Fetching resources from S3 to %s ..." % imgs_dir)
+    s3.fetch_directory(s3_dir=key, local_dir=imgs_dir)
 
-    # print("🔧 Creating configs...")
+    print("🔧 Creating configs...")
+    configsgen.generate_config(imgs_dir, sizes)
+
+    if(type == 'WINDOW'):
+        print("📦 Creating Window bundle...")
+        clickgen.main(name='foo', config_dir=imgs_dir,
+                      out_path='.', win=True, archive=True)
+    elif(type == 'LINUX'):
+        print("📦 Creating Linux bundle...")
+        clickgen.main(name='foo', config_dir=imgs_dir,
+                      out_path='.', x11=True, archive=True)
+    else:
+        print("📦 Creating all types bundle...")
 
     # bundle_url = s3.upload_file_temp(bundle_path, key)
     print('🧹 Cleaning resources..')
-    shutil.rmtree(dir)
+    shutil.rmtree(imgs_dir)
 
     response = {
         "statusCode": 200,
