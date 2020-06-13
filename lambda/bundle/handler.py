d81779a21f8b44e7823a46af9f59ad8af52d2070
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-13 08:05:02 +0530

    📦 upload package to s3 temporary

--- lambda/bundle/handler.py
+++ lambda/bundle/handler.py
@@ -10,12 +10,13 @@ import s3
 
 
 def bundle(event, context):
-
+    name = event["name"]
     key = event["key"]
     type = event["type"]
     sizes = event["sizes"]
 
     imgs_dir = tempfile.mkdtemp()
+    out_dir = tempfile.mkdtemp()
 
     print("🚛 Fetching resources from S3 to %s ..." % imgs_dir)
     s3.fetch_directory(s3_dir=key, local_dir=imgs_dir)
@@ -25,23 +26,26 @@ def bundle(event, context):
 
     if(type == 'WINDOW'):
         print("📦 Creating Window bundle...")
-        clickgen.main(name='foo', config_dir=imgs_dir,
-                      out_path='.', win=True, archive=True)
+        clickgen.main(name, config_dir=imgs_dir,
+                      out_path=out_dir, win=True, archive=True)
     elif(type == 'LINUX'):
         print("📦 Creating Linux bundle...")
-        clickgen.main(name='foo', config_dir=imgs_dir,
-                      out_path='.', x11=True, archive=True)
+        clickgen.main(name, config_dir=imgs_dir,
+                      out_path=out_dir, x11=True, archive=True)
     else:
         print("📦 Creating all types bundle...")
+        clickgen.main(name, config_dir=imgs_dir,
+                      out_path=out_dir, x11=True, win=True, archive=True)
 
-    # bundle_url = s3.upload_file_temp(bundle_path, key)
+    bundle_path = os.path.join(out_dir, name+'.tar')
+    bundle_url = s3.upload_file_temp(bundle_path, key)
+
     print('🧹 Cleaning resources..')
     shutil.rmtree(imgs_dir)
 
     response = {
         "statusCode": 200,
-        # "url": json.dumps(bundle_url)
-        "url": "hurray!"
+        "url": json.dumps(bundle_url)
     }
 
     return response
