8ca95aae5409c2f6d80c531634c5b9a75a5fd775
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-14 17:37:37 +0530

    🏷 typing added

--- lambda/bundle/configsgen/__main__.py
+++ lambda/bundle/configsgen/__main__.py
@@ -8,7 +8,7 @@ from .types import List, Num
 from . import helpers
 
 
-def generate_config(imgs_dir: str, cursor_sizes: List[Num]):
+def generate_config(imgs_dir: str, cursor_sizes: List[Num]) -> str:
     """ Generate helpers files."""
     helpers.generate_static_cursor(imgs_dir, cursor_sizes)
     helpers.generate_animated_cursor(imgs_dir, cursor_sizes)
