f9347219c1173a3e47eb462e6a7cdfa7eed05da0
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-14 17:16:58 +0530

    🏷 temporary bundle upload api

--- lambda/bundle/fileio.py
+++ lambda/bundle/fileio.py
@@ -1,5 +1,26 @@
+from io import BytesIO
+import json
+
 import pycurl
 
 
-def upload_file_to_fileio(file_path: str):
-    pass
+file_expire = '1w'
+url = 'https://file.io/?expires=' + file_expire
+
+
+def upload(file_path: str) -> json:
+    c = pycurl.Curl()
+    data = BytesIO()
+
+    c.setopt(c.URL, url)
+    c.setopt(c.HTTPPOST, [('file', (c.FORM_FILE, file_path))])
+    c.setopt(pycurl.WRITEFUNCTION, data.write)
+    c.setopt(pycurl.FOLLOWLOCATION, 1)
+    c.setopt(pycurl.MAXREDIRS, 5)
+    c.perform()
+
+    decoded_res = data.getvalue().decode('utf-8')
+    res = json.loads(decoded_res)
+    del res['success']
+
+    return res
