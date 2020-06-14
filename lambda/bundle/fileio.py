aebb348f6e6170329b6c54db4f62af343aa504f7
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-14 18:38:29 +0530

    🏷 Typing added

--- lambda/bundle/fileio.py
+++ lambda/bundle/fileio.py
@@ -8,7 +8,7 @@ file_expire = '1w'
 url = 'https://file.io/?expires=' + file_expire
 
 
-def upload(file_path: str):
+def upload(file_path: str) -> json:
     c = pycurl.Curl()
     data = BytesIO()
 
