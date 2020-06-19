3a8b440513aba9e227c7af431ad694885ed78402
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-19 17:31:02 +0530

    💬 comments for hotspots

--- lambda/bundle/configsgen/__main__.py
+++ lambda/bundle/configsgen/__main__.py
@@ -9,7 +9,10 @@ from . import helpers
 
 
 def generate_config(imgs_dir: str, cursor_sizes: List[Num], hotspots: any) -> str:
-    """ Generate helpers files."""
+    """ 
+        Generate helpers files.
+        hotspots is JSON data for each cursor having xhot & yhot parameters.Provide `None` value set hotspots to middle of cursor.
+    """
     helpers.generate_static_cursor(
         imgs_dir, sizes=cursor_sizes, hotspots=hotspots)
     helpers.generate_animated_cursor(
