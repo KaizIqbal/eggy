c40398c3bdeec07f3032e9ad0f8d301e081faf0c
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-25 18:08:17 +0530

    ⬆ Upload script updated

--- lambda/bundle/fileio.py
+++ lambda/bundle/fileio.py
@@ -1,9 +1,16 @@
 import json
-
+import requests
 
 file_expire = '1w'
 url = 'https://file.io/?expires=' + file_expire
 
 
 def upload(file_path: str) -> json:
-    pass
+    files = {
+        'file': open(file_path, 'rb'),
+    }
+    response = requests.post(url, files=files)
+    res = json.loads(response.text)
+    del res['success']
+
+    return res
