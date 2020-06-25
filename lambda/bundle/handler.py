a1456147e39cc5e2bf8544bedaa65e308fdb7de1
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-25 18:30:04 +0530

    🖥 exec with runtime libraries

--- lambda/bundle/handler.py
+++ lambda/bundle/handler.py
@@ -10,6 +10,8 @@ import configsgen
 import fileio
 import helpers
 
+libpath = os.path.abspath('./libraries')
+
 
 def bundle(event, context):
     random_id = helpers.generate_random_id()
@@ -39,12 +41,14 @@ def bundle(event, context):
                           out_path=out_dir, win=True, archive=True, logs=True)
         elif(type == 'LINUX'):
             print("📦 Creating Linux bundle...")
-            clickgen.main(name, config_dir=imgs_dir,
-                          out_path=out_dir, x11=True, archive=True, logs=True)
+            with helpers.LDD(libpath):
+                clickgen.main(name, config_dir=imgs_dir,
+                              out_path=out_dir, x11=True, archive=True, logs=True)
         else:
             print("📦 Creating all types bundle...")
-            clickgen.main(name, config_dir=imgs_dir,
-                          out_path=out_dir, x11=True, win=True, archive=True, logs=True)
+            with helpers.LDD(libpath):
+                clickgen.main(name, config_dir=imgs_dir,
+                              out_path=out_dir, x11=True, win=True, archive=True, logs=True)
 
         # Bundle info
         bundle_name = name+'.tar'
