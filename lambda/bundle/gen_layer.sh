50f4367dbbae9d7d4fd4cbd21d021d614261080f
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-28 19:12:33 +0530

    📦 python packages on layer by requirements.txt

--- lambda/bundle/gen_layer.sh
+++ lambda/bundle/gen_layer.sh
@@ -2,9 +2,12 @@
 
 NAME=dependencies
 
+while IFS= read -r line; do
+  PACKAGES+="$(echo $line | cut -d'=' -f 1) "
+done < requirements.txt
+
 docker run --rm -v "$PWD"/$NAME:/opt lambci/lambda:build-python3.8 \
-    bash -c "yum install -y libX11-devel libpng-devel libXcursor-devel && 
-            pip install clickgen -t /opt/python"
+  bash -c "yum install -y libX11-devel libpng-devel libXcursor-devel && pip install $PACKAGES -t /opt/python"
 
 docker run --rm -v "$PWD"/$NAME:/lambda/opt lambci/yumda:2 \        
   yum install -y libX11 libpng libXcursor
