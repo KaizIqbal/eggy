ea2bd0b5bcc502d6929e44c18c6419f813d325d7
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-30 18:19:13 +0530

    📦 pycurl install with libraries

--- lambda/bundle/gen_layer.sh
+++ lambda/bundle/gen_layer.sh
@@ -4,16 +4,21 @@ NAME=dependencies
 
 while read -r line
 do
-  PACKAGES+="$(echo $line | cut -d'=' -f 1) "
+    name="$(echo $line | cut -d'=' -f 1) "
+    if [ $name != "pycurl" ]; then
+      PACKAGES+=$name
+    fi
 done <<< $(cat requirements.txt)
 
-echo "📦 Packages are installed: ${PACKAGES}"
+echo "📦 Packages are listed in requirements.txt: ${PACKAGES}"
+echo "📦 Pycurl install with following command : export PYCURL_SSL_LIBRARY=openssl && pip install pycurl --no-cache-dir -t /opt/python "
 
 docker run --rm -v "$PWD"/"$NAME":/opt lambci/lambda:build-python3.8 \
-  bash -c "yum install -y libX11-devel libpng-devel libXcursor-devel && pip install ${PACKAGES} -t /opt/python"
+  bash -c "yum install -y libX11-devel libpng-devel libXcursor-devel libcurl-devel && pip install ${PACKAGES} -t /opt/python && export PYCURL_SSL_LIBRARY=openssl && pip install pycurl --no-cache-dir -t /opt/python"
 
-docker run --rm -v "$PWD"/"$NAME":/lambda/opt lambci/yumda:2 yum install -y libX11 libpng libXcursor
+docker run --rm -v "$PWD"/"$NAME":/lambda/opt lambci/yumda:2 yum install -y libX11 libpng libXcursor libcurl openssl gnutls
 
 cd $NAME 
+sudo cp "$NAME/python/Pillow.libs/libpng16-bedcb7ea.so.16.37.0" "$NAME/lib/libpng16.so.16"
 zip -yr ../$NAME.zip .
 cd ..
