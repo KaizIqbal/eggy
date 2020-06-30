3743b1325059de32669bb18ce62cb3cb7ab7ed79
KaizIqbal <24286590+KaizIqbal@users.noreply.github.com>
2020-06-30 17:13:07 +0530

    ⬆ File.io uploading with pycurl

--- lambda/bundle/fileio.py
+++ lambda/bundle/fileio.py
@@ -1,16 +1,27 @@
+from io import BytesIO
 import json
-import requests
+
+import pycurl
+
 
 file_expire = '1w'
 url = 'https://file.io/?expires=' + file_expire
 
 
 def upload(file_path: str) -> json:
-    files = {
-        'file': open(file_path, 'rb'),
-    }
-    response = requests.post(url, files=files)
-    res = json.loads(response.text)
+    c = pycurl.Curl()
+    data = BytesIO()
+
+    c.setopt(c.URL, url)
+    c.setopt(c.HTTPPOST, [('file', (c.FORM_FILE, file_path))])
+    c.setopt(pycurl.WRITEFUNCTION, data.write)
+    c.setopt(pycurl.FOLLOWLOCATION, 1)
+    c.setopt(pycurl.MAXREDIRS, 5)
+    c.perform()
+
+    stream_value = data.getvalue()
+    decoded_res = stream_value.decode('utf-8')
+    res = json.loads(decoded_res)
     del res['success']
 
     return res
